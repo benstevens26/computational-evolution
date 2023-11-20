b6d4654f7cd2119343ffc2607a8004c084aeab44
Ben <benstevens2609@gmail.com>
2023-11-20 17:55:43 +0000

    fixed predator inheritance

--- computational-evolution/code/predator.py
+++ computational-evolution/code/predator.py
@@ -8,28 +8,7 @@ import numpy as np
 
 class Predator(Agent):
     def __init__(self, pos, speed=None, size=None, energy=None):
-
-        if isinstance(pos, list):
-            pos = np.asarray(pos)
-
-        if speed is None:
-            self.speed = np.random.randint(50, 150)
-        else:
-            self.speed = speed
-
-        if size is None:
-            self.size = np.random.randint(100, 200)
-        else:
-            self.size = size
-
-        if energy is None:
-            self.energy = INIT_ENERGY
-        else:
-            self.energy = energy
-
-        self.pos = pos
-        self.direction = np.random.uniform(-np.pi, np.pi)
-        self.rep_threshold = REP_THRESHOLD
+        super().__init__(pos, speed, size, energy)
         self.patch = patches.Circle(self.pos, self.size, fc='red')
 
 
