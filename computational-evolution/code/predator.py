f70353981517136fe11d148b184dd314211d3ac7
Ben <benstevens2609@gmail.com>
2023-11-27 15:23:24 +0000

    added custom eat_food

--- computational-evolution/code/predator.py
+++ computational-evolution/code/predator.py
@@ -9,8 +9,19 @@ import numpy as np
 class Predator(Agent):
     def __init__(self, pos, speed=None, size=None, energy=None, theta=None):
         super().__init__(pos, speed, size, energy, theta)
+
         self.patch = patches.Circle(self.pos, self.size, fc='red')
         self.vision_patch = patches.Wedge(self.pos, self.radius,
                                           theta1=(180 / np.pi) * (self.direction - self.angle / 2),
                                           theta2=(180 / np.pi) * (self.direction + self.angle / 2), alpha=0.3,
                                           fc='lightcoral')
+
+
+    def eat_food(self, food):
+        """Increase agent energy by energy of food"""
+        eat_energy = 1.5 * EAT_ENERGY
+        new_energy = self.get_energy() + eat_energy
+        self.set_energy(new_energy)
+
+        if self.energy > MAX_ENERGY:
+            self.set_energy(MAX_ENERGY)
